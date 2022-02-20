07c792911b5ee9bbef1d05e4aa75ebcca60aed5a
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2022-02-20 12:20:19 +0300

    threading

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -4,17 +4,26 @@ import os
 import logging
 from urllib.parse import urlparse
 from urllib.request import urlretrieve
+import threading
 
 import PIL
 from PIL import Image
 
-logging.basicConfig(filename='logfile.log', level=logging.DEBUG, force=True)
+FORMAT = '[%(threadName)s, %(asctime)s, %(levelname)s] %(message)s'
+logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format=FORMAT, force=True)
 
 class ThumbnailMakerService(object):
     def __init__(self, home_dir='.'):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
+
+    def download_image(self, url):
+        # download each image and save to the input dir
+        logging.info('downloading image at URl ' + url)
+        img_filename = urlparse(url).path.split('/')[-1]
+        urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+        logging.info('image saved to ' + self.input_dir + os.path.sep + img_filename)
 
     # IO-bound method
     def download_images(self, img_url_list):
@@ -26,10 +35,15 @@ class ThumbnailMakerService(object):
         logging.info("beginning image downloads")
 
         start = time.perf_counter()
+        threads = []
         for url in img_url_list:
-            # download each image and save to the input dir 
-            img_filename = urlparse(url).path.split('/')[-1]
-            urlretrieve(url, self.input_dir + os.path.sep + img_filename)
+            t = threading.Thread(target=self.download_image, args=(url,))
+            t.start()
+            # Calling t.join() here will be a mistake, because what would happen is that in a loop, the main thread will create a new thread, start it, and then wait it to complete before continuing the loop, this isn't what we want. What we want is to create the worker threads, and then wait for all of them to complete. So to accomplish that, we'll create a list that will hold references to the thread objects and then every time we create a thread in the loop, we'll add it to the list.
+            threads.append(t)
+        # Then after all the threads are started, we'll going to loop through our thread list and call join on each of the threads, so the main thread is forced to wait for all the threads to complete before continuing.
+        for t in threads:
+            t.join()
         end = time.perf_counter()
 
         logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
@@ -71,7 +85,7 @@ class ThumbnailMakerService(object):
         start = time.perf_counter()
 
         self.download_images(img_url_list)
-        self.perform_resizing()
+        # self.perform_resizing()
 
         end = time.perf_counter()
         logging.info("END make_thumbnails in {} seconds".format(end - start))
